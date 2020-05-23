fa97588f35c6b7bd128e9824bcfc486c52f389bc
Marc-André Larocque Blais <marcandrelarocqueblais@gmail.com>
2020-05-23 17:24:49 -0400

    Joueur basic setup

--- BJ_S/Joueurs.cs
+++ BJ_S/Joueurs.cs
@@ -8,5 +8,34 @@ namespace BJ_S
 {
     class Joueurs
     {
+        string m_Nom;
+        double m_Encaisse;
+        Connexions m_Connection;
+        Mains m_Main;
+
+        public Joueurs(string p_Nom = "Anonyme", bool enLigne = false) 
+        {
+            if (p_Nom.Equals("Anonyme"))
+                m_Nom = $"{p_Nom}{GenererAnonymat()}";
+            else
+                m_Nom = p_Nom;
+
+            m_Encaisse = 500.00;
+            m_Main = new Mains();
+
+            if (enLigne)
+                m_Connection = new Connexions();
+        }
+
+        int GenererAnonymat() 
+        {
+            Random rnd = new Random();
+            return rnd.Next(1, 1000);
+        }
+
+        public string Nom 
+        {
+            get { return m_Nom; }
+        }
     }
 }
