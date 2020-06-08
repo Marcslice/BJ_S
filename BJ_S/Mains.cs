e747cb3c3a9e55fa318d1666969ce017e4ef8af1
Marc-André Larocque Blais <marcandrelarocqueblais@gmail.com>
2020-06-08 14:43:14 -0400

    adding main commenté

--- BJ_S/Mains.cs
+++ BJ_S/Mains.cs
@@ -12,19 +12,20 @@ namespace BJ_S
             main = new List<Cartes>();
         }
 
-        //partie feed une carte a joueur en appelant RecevoirCarte
+        /// <summary>
+        /// Methode qui distribue des carte dans une main
+        /// </summary>
+        /// <param name="nouvelleCarte">Correspond a une carte</param>
         public void RecevoirCarte(Cartes nouvelleCarte)
         {
             main.Add(nouvelleCarte);
         }
 
-        //a modifier pour gerer la carte cacher du croupier
-        public void RecevoirCarte(Cartes nouvelleCarte, bool cache)
-        {
-            main.Add(nouvelleCarte);
-            //ajout qqch ici ou a lautre constructeur
-        }
-
+        /// <summary>
+        /// Methode qui calcule la valeur d'une main en prenant compte de la presence d'as. Advenant que la valeur 
+        /// depasse 21 elle effectue une convertion des as de 11 a 1
+        /// </summary>
+        /// <returns>Retourne un int correspondant a la somme des cartes</returns>
         public int Compte()
         {
             int compte = 0;
@@ -58,11 +59,20 @@ namespace BJ_S
             return compte;
         }
 
+        /// <summary>
+        /// indexeur permettant d'acceder a chacune des cartes
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns>Retourne une carte precise de la main</returns>
         public Cartes this[int i]
         {
             get { return main[i]; }
         }
 
+        /// <summary>
+        /// Permet d'obtenir le nombre de carte presente dans la main
+        /// </summary>
+        /// <returns>Retoune un entier equivalent au nombre de carte</returns>
         public int NombresDeCarte()
         {
             return main.Count;
